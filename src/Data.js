64dfa71e9624043e17410546c04b977e99cfc6a8
George <GeorgeGiorgika@gmail.com>
2021-09-10 13:00:38 +0400

    new portfolio add

--- src/Data.js
+++ src/Data.js
@@ -1,47 +1,55 @@
 const PF = process.env.REACT_APP_PUBLIC_FOLDER
+// "imge/"
 export const data = [
     {
         id:1,
-        img:`imge/quiz.jpg`,
+        img:`${PF}penn.png`,
+        Dep: ["React_Redux", "Sass", "Python/Flask, mysql", "wheather API","ყოველდღიურობაში გამოსაყენებელი ჩანიშვნების აპი"],
+        link: "https://gkap.pythonanywhere.com/",
+        name:"Notes"
+    },
+    {
+        id:2,
+        img:`${PF}quiz.jpg`,
         Dep: ["React", "React-Dom", "Sass", "material-ui","ქვიზი სამი კითხვით, დანარჩენის მოფიქრება დამეზარა, დააკლიკე"],
         link: "https://george-giorgi.github.io/modern_quize/",
         name:"Quiz"
     },
     {
-        id:2,
+        id:3,
         img:`${PF}fb.jpg`,
         Dep: ["React", "React-Dom", "Sass", "material-ui","სოციალური გვერდის ვიზუალი, back-end იხილიეთ githubze . mongodb, express"],
         link: "https://george-giorgi.github.io/social_ui/",
         name:"Social"
     },
     {
-        id:3,
+        id:4,
         img:`${PF}7.jpeg`,
         Dep: ["", "comming soon  python/flask"],
         // link: ""
         name: "Blog"
     },
     {
-        id:4,
+        id:5,
         img:`${PF}ad.png`,
         Dep: ["", "comming soon  python/parsing"],
         // link: "https://george-giorgi.github.io/modern_quize/",
         // name: "flask back"
     },
     {
-        id:5,
+        id:6,
         img:`${PF}6.jpeg`,
         Dep: ["", "comming soon"],
         // link: "https://george-giorgi.github.io/modern_quize/"
     },
     {
-        id:6,
-        img:`imge/5.jpeg`,
+        id:7,
+        img:`${PF}5.jpeg`,
         Dep:  ["", "comming soon"],
         // link: "https://george-giorgi.github.io/modern_quize/"
     },
     {
-        id:7,
+        id:8,
         img:`${PF}person.jpeg`,
         Dep:  ["", "comming soon"],
         // link: "https://george-giorgi.github.io/modern_quize/"
